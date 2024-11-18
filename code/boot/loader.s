7305b19b074d45d227bf80ec071a5f304b5ccbd8
SIMple-lives <1305969480@qq.com>
2024-11-18 21:09:05 +0800

    fix show 2 inreal

--- code/boot/loader.s
+++ code/boot/loader.s
@@ -13,7 +13,7 @@
     DATA_STACK_DESC: dd 0x0000FFFF  ; 数据段和栈段描述符的起始地址
                 dd DESC_DATA_HIGH4
 
-VIDEO_DESC: dd 80000007;0x8000000;limit=(0xbffff-0xb8000)/4096=0x7ff     ;显存段描述符
+VIDEO_DESC: dd 0x80000007;0x8000000;limit=(0xbffff-0xb8000)/4096=0x7ff     ;显存段描述符
                 dd DESC_VIDEO_HIGH4 ; 此时dpl为0，表示用户级
 
     GDT_SIZE equ $ - GDT_BASE
@@ -54,7 +54,7 @@ VIDEO_DESC: dd 80000007;0x8000000;limit=(0xbffff-0xb8000)/4096=0x7ff     ;显存
     mov cx ,17 ;cx=字符串长度
     mov ax ,0x1301 ;AH=13H,AL=01h
     mov bx ,0x001f ;页号为0(BH = 0) 蓝底粉红字(BL = 1fh)
-    mov dx ,1800
+    mov dx ,0x1800     ;这里决定输出到第21行，列号为0。也就是最底下一行的地方
     int 0x10 ;调用BIOS
 
 ;------------------进入保护模式-------------------
@@ -63,7 +63,7 @@ VIDEO_DESC: dd 80000007;0x8000000;limit=(0xbffff-0xb8000)/4096=0x7ff     ;显存
 ;3.将cr0的PE位置1，进入保护模式
     ;----------------------- 打开A20地址的A20端口线----------------------
     in al ,0x92     ;   读取A20端口
-    or al,0000_0010B ; 将A20端口线置1设置A20
+    or al ,0000_0010B ; 将A20端口线置1设置A20
     out 0x92 ,al    ;   写回A20端口
 
     ;----------------------- 加载GDT表描述符，设置GDTR----------------------
@@ -75,20 +75,19 @@ VIDEO_DESC: dd 80000007;0x8000000;limit=(0xbffff-0xb8000)/4096=0x7ff     ;显存
     or eax, 0x00000001 ; 设置PE位
     mov cr0, eax ; 进入保护模式
 
-    jmp dword SELECTOR_CODE:p_mode_start ; 跳转到保护模式代码段 刷新流水线
+    jmp SELECTOR_CODE:p_mode_start   ; 跳转到保护模式代码段 刷新流水线
 
 
 [bits 32]
 p_mode_start:
-    mov ax ,SELECTOR_DATA
-    mov ds ,ax
-    mov es ,ax
-    mov ss ,ax
-    mov esp ,LOADER_STACK_TOP
-    mov ax ,SELECTOR_VIDEO
-    mov gs ,ax
+    mov ax,SELECTOR_DATA
+    mov ds,ax
+    mov es,ax
+    mov ss,ax
+    mov esp,LOADER_STACK_TOP
+    mov ax,SELECTOR_VIDEO
+    mov gs,ax
 
     mov byte [gs:160], 'P'
 
     jmp $
-    
