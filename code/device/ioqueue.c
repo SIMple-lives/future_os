5788e8997c1345091dd6751e8ea0f7d8f25ffe2f
SIMple-lives <1305969480@qq.com>
2025-01-18 21:37:32 +0800

    实现用户环形缓冲区

---
+++ code/device/ioqueue.c
@@ -0,0 +1,82 @@
+#include "ioqueue.h"
+#include "interrupt.h"
+#include "global.h"
+#include "debug.h"
+
+
+void ioqueue_init(struct ioqueue* ioq) {
+    lock_init(&ioq->lock);    //初始化锁
+    ioq->producer = NULL;    //生产者
+    ioq->consumer = NULL;    //消费者
+    ioq->head = 0;            //缓冲区头
+}
+
+//返回pos在缓冲区中的下一个位置
+static int32_t next_pos (int32_t pos) {
+    return (pos+1)%bufsize;
+}
+
+//bool 判读队列是否已满
+bool ioq_full(struct ioqueue* ioq) {
+    ASSERT(intr_get_status() == INTR_OFF);
+    return next_pos(ioq->head) == ioq->tail;
+}
+
+//判断队列是否为空
+bool ioq_empty(struct ioqueue* ioq) {
+    ASSERT(intr_get_status() == INTR_OFF);
+    return ioq->head == ioq->tail;
+}
+
+//使生产者在此缓冲区等待
+static void ioq_wait(struct task_struct** waiter) {
+    ASSERT(*waiter==NULL && waiter!=NULL);
+    *waiter = running_thread();
+    thread_block(TASK_BLOCKED);
+}
+
+/* 唤醒waiter */
+static void wakeup(struct task_struct** waiter) {
+    ASSERT(*waiter != NULL);
+    thread_unblock(*waiter);
+    *waiter = NULL;
+}
+
+//消费者从ioq中取一个字符
+char ioq_getchar(struct ioqueue* ioq) {
+    ASSERT(intr_get_status() == INTR_OFF);
+
+    while(ioq_empty(ioq)) {
+        lock_acquire(&ioq->lock);
+        ioq_wait(&ioq->consumer);
+        lock_release(&ioq->lock);
+    }
+
+    char byte = ioq->buf[ioq->tail];
+    ioq->tail = next_pos(ioq->tail);
+
+    if(ioq->producer != NULL) {
+      wakeup(&ioq->producer);
+    }
+    return byte;
+}
+
+/* 生产者往ioq队列中写入一个字符byte */
+void ioq_putchar(struct ioqueue* ioq, char byte) {
+    ASSERT(intr_get_status() == INTR_OFF);
+
+    /* 若缓冲区(队列)已经满了,把生产者ioq->producer记为自己,
+     * 为的是当缓冲区里的东西被消费者取完后让消费者知道唤醒哪个生产者,
+     * 也就是唤醒当前线程自己*/
+    while (ioq_full(ioq)) {
+        lock_acquire(&ioq->lock);
+        ioq_wait(&ioq->producer);
+        lock_release(&ioq->lock);
+    }
+    ioq->buf[ioq->head] = byte;      // 把字节放入缓冲区中
+    ioq->head = next_pos(ioq->head); // 把写游标移到下一位置
+
+    if (ioq->consumer != NULL) {
+        wakeup(&ioq->consumer);          // 唤醒消费者
+    }
+}
---
+++ code/device/ioqueue.h
@@ -0,0 +1,29 @@
+#ifndef __DEVICE_IOQUEUE_H
+#define __DEVICE_IOQUEUE_H
+#include "stdint.h"
+#include "../thread/thread.h"
+#include "../thread/sync.h"
+
+#define bufsize 64
+
+//环形队列
+struct ioqueue {
+    //生产者消费者问题
+    struct lock lock;
+
+    //生产者，缓冲区不满就放数据，满了就阻塞
+    struct task_struct* producer;
+
+    //消费者，缓冲区不空就取数据，空了就阻塞
+    struct task_struct* consumer;
+    char buf[bufsize];    //缓冲区
+    int32_t head;
+    int32_t tail;
+};
+
+void ioqueue_init(struct ioqueue* ioq);
+bool ioq_full(struct ioqueue* ioq);
+bool ioq_empty(struct ioqueue* ioq);
+char ioq_getchar(struct ioqueue* ioq);
+void ioq_putchar(struct ioqueue* ioq, char byte);
+#endif
