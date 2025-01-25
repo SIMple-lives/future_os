daa07de608804390baeb19df35c31ca13c020a09
SIMple-lives <1305969480@qq.com>
2025-01-25 23:01:00 +0800

    实现系统调用

--- code/userprog/tss.c
+++ code/userprog/tss.c
@@ -33,7 +33,7 @@ struct tss {
     uint32_t ldt;
     uint32_t trace;
     uint32_t io_base;
-}; 
+};
 static struct tss tss;
 
 //更新tss中的esp0字段的值为pthread的0级 栈指针
@@ -44,7 +44,7 @@ void update_tss_esp(struct task_struct* pthread) {
 //创建gdt描述符
 static struct gdt_desc make_gdt_desc(uint32_t* desc_addr, uint32_t limit, uint8_t attr_low, uint8_t attr_high) {
     //put_str("make_gdt_desc\n");
-    uint32_t desc_base = (uint32_t) desc_addr;
+    uint32_t desc_base = (uint32_t)desc_addr;
     struct gdt_desc desc;
     desc.limit_low_word = limit & 0x0000ffff;
     desc.base_low_word = desc_base & 0x0000ffff;
@@ -65,7 +65,7 @@ void tss_init() {
     tss.ss0 = SELECTOR_K_STACK;
     tss.io_base = tss_size;
 
-    //gdt段基址为0x900,把tss放到第四个位置，也就是0x900 + 0x20 = 0x920的位置 0x20 = 4 * 8  
+    //gdt段基址为0x900,把tss放到第四个位置，也就是0x900 + 0x20 = 0x920的位置 0x20 = 4 * 8
 
     //在gdt中添加dpl为0的tss描述符
     *((struct gdt_desc*)0xc0000920) = make_gdt_desc((uint32_t*)&tss, tss_size-1, TSS_ATTR_LOW, TSS_ATTR_HIGH);
